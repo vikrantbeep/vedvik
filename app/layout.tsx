9bee3d800fd898225dabcc3196aa046c83f8b53d
vikrantbeep <vikrant.bangale07@gmail.com>
2026-06-04 13:21:51 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,15 @@
-import type { Metadata } from "next";
+import { GoogleAnalytics } from '@next/third-parties/google'
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <GoogleAnalytics gaId="G-8WKGFHWWG6" />
+      </body>
+    </html>
+  )
+}import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
