0f263646ef18df2f82af3de98dce9fb4cad60545
vikrantbeep <vikrant.bangale07@gmail.com>
2026-06-07 13:12:19 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,8 +52,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             gtag('config', 'G-8WKGFHWWG6');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+  {`
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "x36lcs1018");
+  `}
+</Script>
       </head>
       <body style={{ background: 'white', backgroundImage: 'none' }}>{children}</body>
+    
     </html>
   );
 }
