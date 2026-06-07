69d85a25e69490edd8cdb5f580c2d9d1dc884fcc
vikrantbeep <vikrant.bangale07@gmail.com>
2026-06-07 11:42:54 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,23 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
+  redirects: async () => [
+    {
+      source: '/solutions/pouch-packing-systems',
+      destination: '/solutions/HFFS',
+      permanent: true,
+    },
+    {
+      source: '/solutions/automatic-unwinder-machines',
+      destination: '/solutions/roller-unwinders',
+      permanent: true,
+    },
+    {
+      source: '/solutions/packaging-machinery-solutions',
+      destination: '/solutions',
+      permanent: true,
+    },
+  ],
 };
 
 export default nextConfig;
